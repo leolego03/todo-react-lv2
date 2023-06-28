eee3e59cacbbeeca89739bda22419fa77a0134ff
mmms2000 <moseskmcmmms@gmail.com>
2023-06-28 13:28:21 +0900

    feat. buttons handler

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { useState } from 'react';
 import { submit } from './redux/modules/todoinput';
+import { doner } from './redux/modules/todoinput';
+import { cancler } from './redux/modules/todoinput';
+import { deleter } from './redux/modules/todoinput';
 
 function App() {
-
 
 const data = useSelector((state)=>{
   return state.todoinput
 })
-
+console.log(data)
 
 // dispatch 가져오기
 const dispatch =useDispatch();
@@ -36,6 +38,21 @@ const bodyHandler = (e) =>{
   setBody(e.target.value)
 }
 
+// button handler 완료
+const doneHandler = (id)=>{
+  dispatch(doner(id))
+}
+
+// button handler 취소
+const cancleHandler =(id)=>{
+  dispatch(cancler(id))
+}
+
+// button handler 삭제
+const deleteHandler =(id)=>{
+  dispatch(deleter(id))
+}
+
   return (
     <>
     <div>
@@ -43,17 +60,48 @@ const bodyHandler = (e) =>{
       내용 :<input type='text' name='body' onChange={bodyHandler} value={body}/>
       <button onClick={buttonHandler}> 추가</button>
     </div>
+    <div>
+    <h2>Working~</h2>
+    <div className='container-list'>
+    <div className='wrapper'> 
     {
-      data.map((item)=>{
-        return (
+    data.filter((item)=> item.done === false).map((item)=>{
+        return (<div key={item.id}>
+          <h3> {item.title}</h3>
+          <p> {item.body}</p>
           <div>
-        <h2>{item.title}</h2>
-         {item.body}
-          </div>
-        
-        )
+            <button onClick={()=>deleteHandler(item.id)}>삭제</button>
+            <button onClick={()=>doneHandler(item.id)}>완료</button>
+            </div>
+          </div>)
       })
     }
+    </div>
+
+    </div>
+    </div>
+    <div>
+    <h2>Done!!</h2>
+    <div className='container-list'>
+    <div className='wrapper'> 
+    {
+    data.filter((item)=> item.done === true).map((item)=>{
+        return (<div key={item.id}>
+          <h3> {item.title}</h3>
+          <p> {item.body}</p>
+          <div>
+            <button onClick={()=>deleteHandler(item.id)}>삭제</button>
+            <button onClick={()=>cancleHandler(item.id)}>취소</button>
+            </div>
+          </div>)
+      })
+    }
+    </div>
+
+    </div>
+    </div>
+
+    
     </>
   
 
--- src/redux/modules/todoinput.js
+++ src/redux/modules/todoinput.js
@@ -1,21 +1,43 @@
 const SUBMIT ="SUBMIT";
+const DONE ="DONE"
+const CANCLE ="CANCLE"
+const DELETE ="DELETE"
 
 
 // action creater
 export const submit =(payload)=>{
-    console.log(payload)
+
     return {
         type: SUBMIT,
         payload:payload,
     }
-   
+}
+
+export const doner =(id) =>{
+    return{
+        type :DONE,
+        payload:id
+    }
+}
+
+export const cancler =(id)=>{
+    return {
+        type : CANCLE,
+        payload:id
+    }
+}
+
+export const deleter =(id)=>{
+    return {
+        type: DELETE,
+        payload:id
+    }
 }
 
 // 초기 상태값(state)
-const initialState = [{
-    title: "리덕스",
-    body:"공부해보자"
-}]
+const initialState = [
+    { id: 1,title: "리덕스",body:"공부해보자", done:false},
+    {id: 2,title: "오마이갓",body:"공부해보자", done:true}]
 
 // 리듀서 : 함수
 // input : state 와 action
@@ -23,9 +45,19 @@ const initialState = [{
 const todoinput =(state = initialState , action)=>{
     switch (action.type){
         case SUBMIT:
-            return [...state,{title: action.payload.title, body:action.payload.body}]
+            return [...state,{id:Math.floor(Math.random()*100), title: action.payload.title, body:action.payload.body, done:false}]
+        case DONE:
+            return state.map((item)=>
+                item.id === action.payload? {...item,done:true} : item
+            )
+        case CANCLE:
+            return state.map((item)=>
+                item.id === action.payload? {...item,done:false} : item
+            );
+        case DELETE:
+            return state.filter((item)=> item.id !== action.payload)
         default:
-        return state;
+            return state;
     }
 };
 
